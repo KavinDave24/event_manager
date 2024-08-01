a6fe79a6da40d237fd7858219002c59b8c70dea9
kavindave <kcd7@njit.edu>
2024-08-01 10:38:09 -0400

    fixed schemas errors

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -1,9 +1,6 @@
-from builtins import ValueError, any, bool, str
-from pydantic import BaseModel, EmailStr, Field, validator, root_validator
+from pydantic import BaseModel, EmailStr, Field, validator, root_validator, ValidationError
 from typing import Optional, List
-from datetime import datetime
-from enum import Enum
-import uuid
+from uuid import UUID, uuid4
 import re
 from app.models.user_model import UserRole
 from app.utils.nickname_gen import generate_nickname
@@ -20,7 +17,7 @@ def validate_url(url: Optional[str]) -> Optional[str]:
 
 class UserBase(BaseModel):
     email: EmailStr = Field(..., example="john.doe@example.com")
-    nickname: Optional[str] = Field(None, min_length=3, pattern=r'^[\w-]+$', example=generate_nickname())
+    nickname: Optional[str] = Field(None, min_length=3, regex=r'^[\w-]+$', example=generate_nickname())
     first_name: Optional[str] = Field(None, example="John")
     last_name: Optional[str] = Field(None, example="Doe")
     bio: Optional[str] = Field(None, example="Experienced software developer specializing in web applications.")
@@ -37,20 +34,19 @@ class UserBase(BaseModel):
 
 
 class UserCreate(UserBase):
-    email: EmailStr = Field(..., example="john.doe@example.com")
     password: str = Field(..., example="Secure*1234")
 
 
-class UserUpdate(UserBase):
+class UserUpdate(BaseModel):
     email: Optional[EmailStr] = Field(None, example="john.doe@example.com")
-    nickname: Optional[str] = Field(None, min_length=3, pattern=r'^[\w-]+$', example="john_doe123")
+    nickname: Optional[str] = Field(None, min_length=3, regex=r'^[\w-]+$', example="john_doe123")
     first_name: Optional[str] = Field(None, example="John")
     last_name: Optional[str] = Field(None, example="Doe")
     bio: Optional[str] = Field(None, example="Experienced software developer specializing in web applications.")
     profile_picture_url: Optional[str] = Field(None, example="https://example.com/profiles/john.jpg")
     linkedin_profile_url: Optional[str] = Field(None, example="https://linkedin.com/in/johndoe")
     github_profile_url: Optional[str] = Field(None, example="https://github.com/johndoe")
-    role: Optional[str] = Field(None, example="AUTHENTICATED")
+    role: Optional[UserRole] = Field(None, example=UserRole.AUTHENTICATED)
 
     @root_validator(pre=True)
     def check_at_least_one_value(cls, values):
@@ -65,15 +61,12 @@ class UserUpdate(UserBase):
 
 
 class UserResponse(UserBase):
-    id: uuid.UUID = Field(..., example=uuid.uuid4())
-    email: EmailStr = Field(..., example="john.doe@example.com")
-    nickname: Optional[str] = Field(None, min_length=3, pattern=r'^[\w-]+$', example=generate_nickname())
+    id: UUID = Field(..., example=uuid4())
     is_professional: Optional[bool] = Field(default=False, example=True)
-    role: UserRole
 
 
 class LoginRequest(BaseModel):
-    email: str = Field(..., example="john.doe@example.com")
+    email: EmailStr = Field(..., example="john.doe@example.com")
     password: str = Field(..., example="Secure*1234")
 
 
@@ -84,12 +77,12 @@ class ErrorResponse(BaseModel):
 
 class UserListResponse(BaseModel):
     items: List[UserResponse] = Field(..., example=[{
-        "id": uuid.uuid4(), "nickname": generate_nickname(), "email": "john.doe@example.com",
-        "first_name": "John", "bio": "Experienced developer", "role": "AUTHENTICATED",
-        "last_name": "Doe", "bio": "Experienced developer", "role": "AUTHENTICATED",
-        "profile_picture_url": "https://example.com/profiles/john.jpg",
+        "id": uuid4(), "nickname": generate_nickname(), "email": "john.doe@example.com",
+        "first_name": "John", "bio": "Experienced developer", "role": UserRole.AUTHENTICATED,
+        "last_name": "Doe", "profile_picture_url": "https://example.com/profiles/john.jpg",
         "linkedin_profile_url": "https://linkedin.com/in/johndoe",
-        "github_profile_url": "https://github.com/johndoe"
+        "github_profile_url": "https://github.com/johndoe",
+        "is_professional": False
     }])
     total: int = Field(..., example=100)
     page: int = Field(..., example=1)
